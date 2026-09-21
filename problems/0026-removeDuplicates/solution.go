5d7006efb52206a70c5da97058f9310df29ba752
reftch <j.developer@hotmail.com>
2026-09-21 20:15:26 +0200

    Code cleanup

--- problems/0026-removeDuplicates/solution.go
+++ problems/0026-removeDuplicates/solution.go
@@ -7,7 +7,8 @@
 //
 // Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing duplicates, return the number of unique elements k.
 //
-// The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
+// The first k elements of nums should contain the unique numbers in sorted order
+// The remaining elements beyond index k - 1 can be ignored
 //
 // Custom Judge:
 //
@@ -22,25 +23,25 @@
 // for (int i = 0; i < k; i++) {
 //     assert nums[i] == expectedNums[i];
 // }
-// If all assertions pass, then your solution will be accepted.
+// If all assertions pass, then your solution will be accepted
 //
 // Example 1:
 //
 // Input: nums = [1,1,2]
 // Output: 2, nums = [1,2,_]
-// Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
-// It does not matter what you leave beyond the returned k (hence they are underscores).
-
+// Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively
+// It does not matter what you leave beyond the returned k (hence they are underscores)
+//
 // Example 2:
 //
 // Input: nums = [0,0,1,1,1,2,2,3,3,4]
 // Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
-// Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
-// It does not matter what you leave beyond the returned k (hence they are underscores).
+// Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively
+// It does not matter what you leave beyond the returned k (hence they are underscores)
 
 package removeDuplicates
 
-// removeDuplicates implements the solution.
+// removeDuplicates implements the solution
 func removeDuplicates(nums []int) int {
 	if len(nums) == 0 {
 		return 0
