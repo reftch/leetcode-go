0ed2b91b1f12da4553c57705b8ed23e2d55a74c6
reftch <j.developer@hotmail.com>
2026-09-21 21:08:02 +0200

    Code cleanup

--- problems/0027-removeElement/solution.go
+++ problems/0027-removeElement/solution.go
@@ -45,10 +45,6 @@ package removeElement
 
 // removeElement implements the solution.
 func removeElement(nums []int, val int) int {
-	if len(nums) == 0 {
-		return 0
-	}
-
 	for i := 0; i < len(nums); i++ {
 		if nums[i] == val {
 			nums = append(nums[:i], nums[i+1:]...)
